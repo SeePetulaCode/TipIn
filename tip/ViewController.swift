a0c43ff693a8a3277e6e08424dee12450622ddbf
“seepetulacode” <petulapascall@gmail.com>
2020-09-05 10:24:48 -0700

    pussh progress

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        
+       //Toggle Keyboard Number
+        view.endEditing(true)
+    
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        //Get the bill amt
+        let bill = Double(billField.text!) ?? 0
+        
+        
+        //Calculate tip and total
+        let tipPercentages = [0.1,0.18, 0.2]
+        let tip = bill * tipPercentages[0]
+        let total = bill + tip
+        
+        
+        //Update tip and total
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 }
 
